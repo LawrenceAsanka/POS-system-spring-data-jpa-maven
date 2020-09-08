b99f228338efea5bc96c8cf0b94e5736c1981013
LawrenceAsanka <asankalorance@gmail.com>
2020-09-08 08:22:01 +0530

    Change query methods in Repository

--- src/main/java/lk/ijse/dep/repository/OrderRepository.java
+++ src/main/java/lk/ijse/dep/repository/OrderRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, String> {
 
-    String getLastOrderId() throws Exception;
+    String getFirstLastOrderIdByOrderByDesc() throws Exception;
 
 }
